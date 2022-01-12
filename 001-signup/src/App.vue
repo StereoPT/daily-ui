621f1f10f37d26401c2a59940152242ba3b8f7bd
Guido Pereira <stereopt@gmail.com>
2022-01-12 17:07:44 +0000

    Working Tabs

--- 001-signup/src/App.vue
+++ 001-signup/src/App.vue
@@ -1,15 +1,57 @@
 <template>
-  <div class="flex h-screen justify-center items-center">
-    <h1 class="text-3xl font-bold underline">
-      001 - Sign Up
-    </h1>
+  <div class="w-full px-10 mx-auto">
+    <div class="h-screen flex justify-center items-center">
+      <!-- Form Container -->
+      <div class="form-container bg-red-500">
+        <div class="flex text-center">
+          <!-- Form Tabs -->
+          <div class="flex-1 py-4 bg-gray-300 cursor-pointer" @click="toggleTab('signin')">
+            <a>Sign In</a>
+          </div>
+          <div class="flex-1 py-4 bg-gray-300 cursor-pointer" @click="toggleTab('signup')">
+            <a>Sign Up</a>
+          </div>
+        </div>
+        <div class="text-center py-10" v-if="signInTab">
+          <!-- Sign In Form -->
+          <h1 class="text-3xl font-bold underline">
+            Sign In
+          </h1>
+        </div>
+        <div class="text-center py-10" v-else>
+          <!-- Sign Up Form -->
+          <h1 class="text-3xl font-bold underline">
+            Sign Up
+          </h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: '001-SignUp',
+  data() {
+    return {
+      signInTab: true
+    }
+  },
+  methods: {
+    toggleTab(btn) {
+      if(btn === 'signin') this.signInTab = true;
+      else if(btn === 'signup') this.signInTab = false;
+    }
+  }
 }
 </script>
 
-<style scoped> </style>
+<style>
+  body {
+    background-color: #dedede;
+  }
+
+  .form-container {
+    min-width: 480px;
+  }
+</style>
