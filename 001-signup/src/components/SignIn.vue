12a9cc6b0c70af2e63889061c739696b161e32fb
Guido Pereira <stereopt@gmail.com>
2022-01-14 12:56:51 +0000

    SignIn Done

--- 001-signup/src/components/SignIn.vue
+++ 001-signup/src/components/SignIn.vue
@@ -3,15 +3,17 @@
     <input class="custom-input user-input" type="text" name="username" id="username" placeholder="Username">
     <input class="custom-input password-input" type="password" name="password" id="password" placeholder="Password">
     <div class="flex justify-between px-2">
-      <div>
+      <div class="custom-checkbox">
         <input type="checkbox" id="remember" name="remember">
-        <label for="remember"> Remember Me</label>
+        <label for="remember">Remember me</label>
       </div>
-      <span>Forgot Password</span>
+      <a class="custom-link">Forgot password</a>
     </div>
-    <button class="btn-login py-4">LOG IN</button>
-    <span>or</span>
-    <button class="btn-login-facebook py-4">Log In with Facebook</button>
+    <button class="btn btn-login py-4">LOG IN</button>
+    <span class="divider">or</span>
+    <button class="btn btn-login-facebook py-4">
+      Log In with Facebook
+    </button>
   </form>
 </template>
 
@@ -45,11 +47,78 @@ export default {
     outline: none;
   }
 
+  .custom-checkbox { 
+    font-size: 12px;
+  }
+
+  .custom-checkbox label { 
+    display: inline-block;
+  }
+
+  #remember { 
+    display: none;
+  }
+
+  .custom-checkbox label:before {
+    content: '';
+
+    cursor: pointer;
+    vertical-align: middle;
+    color: black;
+    background-color: #23383E;
+
+    line-height: 1.1em;
+    font-weight: bold;
+    text-align: center;
+    font-size: 9px;
+    height: 12px;
+    width: 12px;
+    margin-right: 0.25rem;
+    display: inline-block;
+    border: 1px solid #57ffff;
+    box-sizing: border-box;
+  }
+
+  :checked+label:before {
+    content: '\2713';
+    color: #57ffff;
+  }
+
+  .custom-checkbox input {
+    background-color: transparent;
+  }
+
+  .custom-link {
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
   .btn-login {
+    font-size: 16px;
+    font-weight: 800;
     background-color: #fca842;
+    margin: 0 1rem 0 1rem;
+  }
+
+  .divider::before,
+  .divider::after {
+      display: inline-block;
+      content: "";
+      vertical-align: middle;
+      border-top: 1px solid #57ffff;
+      width: 2.5rem;
+      margin: 0 0.75rem;
   }
 
   .btn-login-facebook {
+    font-size: 16px;
+    background-image: url('../assets/f.svg');
     background-color: #2e78be;
+    background-size: 16px;
+    background-position: left 1.5rem center;
+    background-repeat: no-repeat;
+    margin: 0 1rem 0 1rem;
+    padding-left: 1.5rem;
   }
 </style>
