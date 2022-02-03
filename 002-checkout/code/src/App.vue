ddb8b589c7ab1c548cf1c8db357ee8f7ff7b7c60
Guido Pereira <stereopt@gmail.com>
2022-02-03 17:51:34 +0000

    Font, Grid & Background Color

--- 002-checkout/code/src/App.vue
+++ 002-checkout/code/src/App.vue
@@ -1,8 +1,39 @@
 <template>
-  <div class="flex h-screen justify-center items-center">
-    <h1 class="text-3xl font-bold underline">
-      002 - Credit Card Checkout
-    </h1>
+  <div class="w-full px-10 mx-auto">
+    <div class="h-screen flex justify-center items-center">
+      <!-- Form Container -->
+      <div class="flex form-container rounded shadow-lg">
+        <!-- Your Order Container -->
+        <div class="flex-1 custom-bg">
+          <img src="#" alt="">
+          <h4>Your Order</h4>
+          <div>
+            <img src="#" alt="">
+          </div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <h4>Total</h4>
+          <h4>$145.98</h4>
+        </div>
+        <!-- Pay Container -->
+        <div class="flex-1 p-10 grid gap-y-8">
+          <h4 class="uppercase">Pay with Credit Card</h4>
+          <input type="text">
+          <input type="text">
+          <div>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <input type="number" name="" id="">
+          </div>
+          <input type="submit" value="PAY SECURELY">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,4 +43,8 @@ export default {
 }
 </script>
 
-<style scoped> </style>
+<style>
+  .custom-bg {
+    background-color: #f7eee9;
+  }
+</style>
